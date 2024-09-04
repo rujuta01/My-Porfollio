eab0c60b59249f6479ffa374764ba4a81fa79e04
rujuta01 <161105730+rujuta01@users.noreply.github.com>
2024-09-04 12:10:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+/*===========================toggle icon navbar====================*/
+
+let nemuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+/*===========================scroll section activae link====================*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*===========================stickly navbar====================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+    /*===========================remove toggle icon and navbar====================*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+/*===========================scroll bar====================*/
+
+ScrollReveal({
+    distance: '80px',
+    duration:2000,
+    delay:200,
+});
+ScrollReveal().reveal('.home-content', 'heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', { origin: 'button' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content',  { origin: 'right' });
+
+/*============================script js===============*/
+document.addEventListener('DOMContentLoaded', function() {
+    new Typed('#typed-text', {
+        strings: ["Frontend Developer", "Quick Learner", "Creative Designer"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 1500,
+        startDelay: 500,
+        loop: true
+    });
+});
+// script.js
+
+document.getElementById('download-cv-btn').addEventListener('click', function() {
+    alert('Your CV will be downloaded shortly.');
+});
